08b0233554754b2b7a0806b192d3570539ff03e6
Sergiu Dumbrava <sergiu.dumbrava@gmail.com>
2021-05-05 10:29:13 -0500

    add azure storage explorer and node lts

--- devmachine.ps1
+++ devmachine.ps1
@@ -25,12 +25,12 @@ cinst javaruntime -y
 # Dev Tools
 cinst git.install -y
 cinst git-fork -y
-cinst nvm -y
 cinst cascadiacode -y
 cinst vscode -y
 cinst postman -y
 cinst fiddler -y
 cinst azure-cli -y
+choco install microsoftazurestorageexplorer -y
 choco install servicebusexplorer -y
 choco install ssms -y
 
@@ -51,3 +51,9 @@ cinst powertoys -y
 choco install sql-server-2019  --cacheLocation="c:\temp"
 choco install sql-server-2019-cumulative-update  --cacheLocation="c:\temp"
 choco install sql-server-management-studio  --cacheLocation="c:\temp"
+
+# Node
+
+choco install nodejs-lts -y
+choco install python2 -y
+choco install visualstudio2017-workload-vctools -y
